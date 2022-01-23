a7bce83f053979fcb18f308a94dc7125f20521a0
Bartek <bartosz.kk@outlook.com>
2022-01-23 16:09:16 +0100

    usuniety widok visits

--- Hospital/urls.py
+++ Hospital/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from . import views
+from register import views as regViews
 
 #przekierowania do odpowiednich funkcji
 urlpatterns = [
     path('home/', views.home, name='home'),
-    path('myappointment/', views.appointment),
+    path('opinions/', views.addOpinionForm, name='opinions'),
+    path('contact/', views.contact, name='contact'),
+    path('myappointment/', views.appointment2),
     path('addDoctor/', views.addDoctorForm),
+    path('addAppointment/', views.addAppointment),
+    path('myappointment/cancel/<int:id>/', views.cancelAppointment),
     path('', views.home)
-    
 ]
--- Hospital/views.py
+++ Hospital/views.py
@@ -1,16 +1,37 @@
 from email import message
 from multiprocessing import context
+from random import sample
 from django.shortcuts import render, redirect
-from .forms import DoctorForm
+
+from .models import Appointment, Doctor, Opinion, STATUS_OF_VISIT
+from .forms import AppointmentForm, DoctorForm, OpinionForm
 
 
 
 def home(request):
-    return render(request, 'home.html')
+    doctors =list(Doctor.objects.all())#pobieranie wszystkich lekarzy
+    numberOfDoctors = len(doctors)
+    if numberOfDoctors >= 3:
+        doctors_3 = sample(doctors,3)# pobieranie 3 losowych lekarzy do wyświetlenia
+    else:
+        doctors_3 = sample(doctors, numberOfDoctors)
+    if request.user.is_authenticated: # sprawdzenie czy użytkownik jest zalogowany i zwraca odpowiedni widok
+        return render(request, 'home.html', {'doctors':doctors_3})
+    else:
+        return render(request, 'home_unauthenticated.html', {'doctors':doctors_3})
 
-def appointment(request):
+
+def opinions(request):
+    return render(request, 'opinions.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def appointment2(request):
     if request.user.is_authenticated:
-        return render(request, 'myappointment.html', {})
+        appointments = Appointment.objects.filter(created_by=request.user)#filtrowanie danych z bazy według użytkownika który wysłał zapytanie
+        return render(request, 'myappointment.html', {'appointments':appointments})
     else:
         return redirect('/home')
 
@@ -19,7 +40,55 @@ def addDoctorForm(request):
     if request.method == 'POST':
         print(request.POST)
         form= DoctorForm(request.POST)
-        if form.is_valid():
+        if form.is_valid() and request.user.is_superuser:
             form.save()
     context = {'form':form}
-    return render(request, 'addDoctor.html', context)
+    if(request.user.is_superuser):
+        return render(request, 'addDoctor.html', context)
+    else:
+        return redirect('/home')
+
+def addOpinionForm(request):
+    form = OpinionForm()
+    opinions = Opinion.objects.all()
+    if request.method == 'POST':
+        print(request.POST)
+        form = OpinionForm(request.POST)
+        if form.is_valid() and request.user.is_authenticated:
+            obje = form.save(commit=False)
+            obje.created_by = request.user
+            form.save()
+    context = {'form':form, 'opinions': opinions}
+    return render(request, 'opinions.html', context)
+
+def addAppointment(request):
+    form = AppointmentForm()
+    if request.method == 'POST':
+        print(request.POST)
+        form = AppointmentForm(request.POST, request.FILES)
+        if form.is_valid() and request.user.is_authenticated:
+            obje = form.save(commit=False) #pobranie obiektu z formularza i przypisanie mu odpowiednich danych
+            obje.created_by = request.user
+            #obje.status = STATUS_OF_VISIT[0]
+            #obje.status_of_visit = STATUS_OF_VISIT['Anulowana']
+            obje.status_of_visit = 'Oczekuje na akceptacje'
+            form.save()
+    context = {'form':form}
+    return render(request, 'addAppointment.html', context)
+
+def cancelAppointment(request, id):
+    if request.user.is_authenticated:
+        try:
+            appointment = Appointment.objects.get(id=id)#zwrócenie wizyt wizyt danego użytkownika
+        except Appointment.DoesNotExist:
+            return render(request, appointment2)
+        if(appointment.created_by == request.user):
+            appointment.status_of_visit = 'Anulowana'
+            appointment.save()
+            appointments = Appointment.objects.filter(created_by=request.user)
+            return render(request, 'myappointment.html', {'appointments':appointments})
+        else:
+            appointments = Appointment.objects.filter(created_by=request.user)
+            return render(request, 'myappointment.html', {'appointments':appointments})
+    else:
+        return render(request, 'home.html')
